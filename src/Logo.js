731a94f4e4ad3fbe8865194c504f3dcff7ed3cac
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-04 16:22:45 +0800

    Added in Open Sans font family

---
+++ src/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SpinachPic from "./images/modern-spinach1.png"
+
+export default function Logo(){
+    return (
+        <div>
+            <img src={SpinachPic} className="SpinachLogo" alt="logo" />
+        </div>
+    )
+}
