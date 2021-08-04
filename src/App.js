6166f1db653a2dfc0bfe205897548f1832de9938
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-04 18:36:04 +0800

    app styling and minor edits

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import About from './About'
 import Cart from './Cart'
 import Logo from './Logo'
 import Menu from './Menu'
+import Userbutton from './Userbutton'
 
 function App() {
   return (
@@ -19,9 +20,12 @@ function App() {
       <div className="App">
         <div className="NavBar">
               <Link to="/"><Logo to="/"/></Link>
-              <Link className="NavLink" to="/menu">Menu</Link>
-              <Link className="NavLink" to="/cart">Cart</Link>
-              <Link className="NavLink" to="/about">About</Link>
+              <div className="CentralNavBar">
+                <Link className="NavLink" to="/menu">Menu</Link>
+                <Link className="NavLink" to="/cart">Cart</Link>
+                <Link className="NavLink" to="/about">About</Link>
+              </div>
+              <Userbutton/>
         </div>
         <Switch>
           <Route path="/menu">
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
+import "./Home.css"
+import Slideshow from "./Slideshow"
 
 export default function Home() {
     return (
-        <div>home</div>
+        <div>
+            <Slideshow/>
+        </div>
     )
 }
