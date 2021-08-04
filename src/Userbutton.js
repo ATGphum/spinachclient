49a799157901c19d1bc30976e6365fc21c4d4701
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-04 18:35:42 +0800

    css styling for user button

---
+++ src/Userbutton.js
@@ -0,0 +1,8 @@
+import Usericon from "./vectors/login.png"
+import "./Userbutton.css"
+
+export default function UserButton(){
+    return (
+            <img src={Usericon} className="Usericon" alt="usericon" />
+    )
+}
