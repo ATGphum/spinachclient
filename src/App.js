18f478520f11b65ddc98acdf5008b93d8ea91339
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-06 19:02:21 +0800

    Added scroll button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,17 +15,37 @@ import Menu from './Menu'
 import Userbutton from './Userbutton'
 
 function App() {
+  const [show, setShow] = useState(true)
+  const [lastScrollPosition, setScrollPosition] = useState(0)
+
+  const controlNavbar = () => {
+    let currentPosition = window.scrollY
+    if (currentPosition > lastScrollPosition) {
+      setShow(false)
+    } else {
+      setShow(true)
+    }
+    setScrollPosition(currentPosition)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar)
+    return () => {
+      window.removeEventListener('scroll', controlNavbar)
+    }
+  }, [])
+
   return (
     <Router>
       <div className="App">
-        <div className="NavBar">
-              <Link to="/"><Logo to="/"/></Link>
-              <div className="CentralNavBar">
-                <Link className="NavLink" to="/menu">Menu</Link>
-                <Link className="NavLink" to="/cart">Cart</Link>
-                <Link className="NavLink" to="/about">About</Link>
-              </div>
-              <Userbutton/>
+        <div className={`NavBar ${show && 'NavBarShow'}`}>
+          <Link to="/"><Logo to="/" /></Link>
+          <div className="CentralNavBar">
+            <Link className="NavLink" to="/menu">Menu</Link>
+            <Link className="NavLink" to="/cart">Cart</Link>
+            <Link className="NavLink" to="/about">About</Link>
+          </div>
+          <Userbutton />
         </div>
         <Switch>
           <Route path="/menu">
--- src/Slideshow.js
+++ src/Slideshow.js
@@ -2,6 +2,7 @@ import "./Slideshow.css"
 import LuPull from "./images/side-lu.jpg"
 import RefreshingRiver from "./images/refreshing-river.jpg"
 import Statistics from "./images/statistics.jpg"
+import React, { useState, useEffect } from "react";
 
 function Slide(props){
     let phrase = props.phrase
@@ -9,14 +10,39 @@ function Slide(props){
     return (
         <div style={{backgroundImage: "url(" + image + ")"}} className="Slide">
             <div className="SlideText">{phrase}</div>
+            <ScrollText/>
         </div>
     )
 }
 
+function ScrollText(){
+    return (
+        <span className="ScrollText">SCROLL DOWN</span>
+    )
+}
+
 export default function Slideshow() {
+
     let imageArray = [LuPull, RefreshingRiver, Statistics]
     let textArray = ["Can take you past your natural limit for half natty results, at none of the cost", "Naturally found and extracted from spinach, vegan diet ftw", "Shown in lab data to yield superior results to Dianabol, with no side effectz"]
+
+    const [slideImage, setImage] = useState(imageArray[0]);
+    const [slideText, setText] = useState(textArray[0]);
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+
+        let updateSlide = () => {
+            setCurrentSlide((currentSlide + 1) % 3)
+            setImage(imageArray[currentSlide])
+            setText(textArray[currentSlide])
+        }
+
+        window.setTimeout(updateSlide, 5000)
+    })
+
+
     return (
-        <Slide image={imageArray[2]} phrase={textArray[0]}/>
+        <Slide image={slideImage} phrase={slideText}/>
     )
 }
