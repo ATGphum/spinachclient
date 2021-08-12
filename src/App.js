10e3d5f06a612975a4f6dd5e7a612996466bc91a
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-12 18:23:48 +0800

    Moved storage for bag into app module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React, { useState }from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,11 +9,99 @@ import {
 import NavBar from './NavBar'
 import Home from './HomePage/Home'
 import About from './About'
-import Bag from './Bag'
+import ShoppingBag from './Bag'
 import Menu from './Menu/Menu'
 import BottomBar from './BottomBar'
 
 function App() {
+
+  let itemList = [
+    { 
+        id: 0,
+        title: "Chilsung soju",
+        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
+        price: 19,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 1,
+        title: "Andong soju",
+        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
+        price: 6,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 2,
+        title: "Churum soju",
+        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
+        price: 10,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 3,
+        title: "Philippe soju",
+        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
+        price: 19,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 4,
+        title: "Phum soju",
+        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
+        price: 6,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 5,
+        title: "Tent soju",
+        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
+        price: 10,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 6,
+        title: "Xiaojun soju",
+        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
+        price: 19,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 7,
+        title: "Korean soju",
+        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
+        price: 6,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+    { 
+        id: 8,
+        title: "Eugene soju",
+        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
+        price: 10,
+        stock: 10,
+        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
+    },
+  ];
+
+  const bagStruct = itemList.map((item) => 
+({
+    id: item.id,
+    title: item.title,
+    imageUrl: item.imageUrl,
+    price: item.price,
+    stock: item.stock,
+    description: item.description,
+    quantity: 0
+  }));
+
+  const [bag, setBag] = useState(bagStruct)
 
   return (
     <Router>
@@ -21,13 +109,13 @@ function App() {
         <NavBar/>
         <Switch>
           <Route path="/menu">
-            <Menu />
+            <Menu bag={bag} setBag={setBag}/>
           </Route>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/bag">
-            <Bag />
+            <ShoppingBag bag={bag} setBag={setBag}/>
           </Route>
           <Route path="/">
             <Home />
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -6,95 +6,13 @@ import MenuBox from "./MenuBox"
 import BagAlert from "./BagAlert"
 
 //<PageHeader headerColor={"#999"} headerContent={"Introducing Oppa, Australia's new and exciting soju startup."}/>
-let itemList = [
-    { 
-        id: 0,
-        title: "Chilsung soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 1,
-        title: "Andong soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 2,
-        title: "Churum soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 3,
-        title: "Philippe soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 4,
-        title: "Phum soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 5,
-        title: "Tent soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 6,
-        title: "Xiaojun soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 7,
-        title: "Korean soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-    { 
-        id: 8,
-        title: "Eugene soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart."
-    },
-];
 
-export default function Menu() {
 
-    const bagStruct = itemList.map((item) => 
-        ({
-            id: item.id,
-            title: item.title,
-            imageUrl: item.imageUrl,
-            price: item.price,
-            stock: item.stock,
-            description: item.description,
-            quantity: 0
-        }));
+export default function Menu(props) {
+    let bag = props.bag;
+    let setBag = props.setBag;
 
-    const [bag, setBag] = useState(bagStruct)
+
     const [lastModifiedItem, setLastModifiedItem] = useState(-1)
     const [bagAlert, setBagAlert] = useState(<div></div>)
     const [showAlert, setShowAlert] = useState(false)
@@ -119,7 +37,10 @@ export default function Menu() {
         if(bagAlertMounted.current){
             setBagAlert(<BagAlert item={lastModifiedItem}/>);
             setShowAlert(true)
-            window.setTimeout(() => {if(!showAlert)setShowAlert(false)}, 3000)
+
+            sessionStorage.setItem('bag', JSON.stringify(bag)) //store bag in session storage
+
+            window.setTimeout(() => {if(!showAlert)setShowAlert(false)}, 3000)//fade the alert after 3 seconds, but only if state variable is false
         }
         else{
             bagAlertMounted.current = true;
