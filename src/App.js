4616d4f4300b3cd83d9551100fde39330fd510b7
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-08-29 18:07:11 +0800

    moved product list into api call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState }from "react";
+import React, { useState, useEffect }from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,107 +15,28 @@ import BottomBar from './BottomBar'
 
 function App() {
 
-  let itemList = [
-    { 
-        title: "Chilsung soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        miscimageUrl: "https://i0.wp.com/www.plantnet.com.au/wp-content/uploads/plantnet-rootstock-plum-Dwarf-Damson.jpg?fit=2114%2C1400&ssl=1",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 1,
-        title: "Andong soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        miscimageUrl: "https://www.carvingajourney.com/wp-content/uploads/2021/07/Onggi002-750x500.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 2,
-        title: "Churum soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        miscimageUrl: "https://cdn.vox-cdn.com/thumbor/RlvyGHIno4ECic9rMOke9_psDQM=/160x0:1120x720/1200x800/filters:focal(160x0:1120x720)/cdn.vox-cdn.com/uploads/chorus_image/image/55904861/1_TQEtyXxEhY-p_vj1bOTIng.0.jpeg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 3,
-        title: "Philippe soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        miscimageUrl: "https://media.gq.com/photos/5bd0a982039397509ca2f67d/16:9/w_1280,c_limit/drink-soju-gq.jpg",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 4,
-        title: "Phum soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 5,
-        title: "Tent soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 6,
-        title: "Xiaojun soju",
-        imageUrl: "https://justliquor.com.au/1080-large_default/chamisul-soju.jpg",
-        price: 19,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 7,
-        title: "Korean soju",
-        imageUrl: "https://justliquor.com.au/1015-large_default/good-day-soju-citron.jpg",
-        price: 6,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-    { 
-        id: 8,
-        title: "Eugene soju",
-        imageUrl: "https://justliquor.com.au/2776-large_default/chum-churum-original-soju.jpg",
-        price: 10,
-        stock: 10,
-        description: "Sweet green grape meets morning dew for the best flavor. Fruit juice in soju is becoming more popular, and Jinro Green Grape has unique green grape flavor and taste that everyone can enjoy. Easy to drink with an alcohol content of 13%, it has such a clean and fresh flavor that will capture your heart.",
-        branddescription: "Chilsung is known for its amazing quality and care"
-    },
-  ];
+  const [bag, setBag] = useState([])
 
-  const bagStruct = itemList.map((item) => 
-({
-    id: item.id,
-    title: item.title,
-    imageUrl: item.imageUrl,
-    miscimageUrl: item.miscimageUrl,
-    price: item.price,
-    stock: item.stock,
-    description: item.description,
-    branddescription: item.branddescription,
-    quantity: 0
-  }));
+  useEffect(() => {
 
-  const [bag, setBag] = useState(bagStruct)
+    fetch("http://127.0.0.1:8000/product/")
+      .then(res => res.json())
+      .then(result => {return result.map((item) => 
+        ({
+          title: item.title,
+          imageUrl: item.imageUrl,
+          miscimageUrl: item.miscimageUrl,
+          price: item.price,
+          stock: item.stock,
+          description: item.description,
+          branddescription: item.branddescription,
+          quantity: 0
+        }));
+      })
+      .then(result => setBag(result))
+      .catch(err => console.log(err))
+
+  }, [])
 
   return (
     <Router>
